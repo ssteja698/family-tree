b1259ba5d770e8cc31fc345eb41cccfd27e1dd6d
Sai Siva Teja Bejjipuram <sai.bejjipuram@leadsquared.com>
2022-07-19 10:06:52 +0530

    make it expandable

--- src/components/familyTree.js
+++ src/components/familyTree.js
@@ -1,99 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 import { family } from "../constants/familyData";
 
-const FamilyTree = () => {
-  const familyColors = {};
-
-  function generateNewColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function generateNewColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
 
-  function padZero(str, len) {
-    len = len || 2;
-    var zeros = new Array(len).join("0");
-    return (zeros + str).slice(-len);
+function padZero(str, len) {
+  len = len || 2;
+  var zeros = new Array(len).join("0");
+  return (zeros + str).slice(-len);
+}
+
+function invertColor(hex, bw) {
+  if (hex.indexOf("#") === 0) {
+    hex = hex.slice(1);
   }
-
-  function invertColor(hex, bw) {
-    if (hex.indexOf("#") === 0) {
-      hex = hex.slice(1);
-    }
-    if (hex.length === 3) {
-      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-      throw new Error("Invalid HEX color.");
-    }
-    var r = parseInt(hex.slice(0, 2), 16),
-      g = parseInt(hex.slice(2, 4), 16),
-      b = parseInt(hex.slice(4, 6), 16);
-    if (bw) {
-      return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#ffffff";
-    }
-    r = (255 - r).toString(16);
-    g = (255 - g).toString(16);
-    b = (255 - b).toString(16);
-    return "#" + padZero(r) + padZero(g) + padZero(b);
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
   }
+  if (hex.length !== 6) {
+    throw new Error("Invalid HEX color.");
+  }
+  var r = parseInt(hex.slice(0, 2), 16),
+    g = parseInt(hex.slice(2, 4), 16),
+    b = parseInt(hex.slice(4, 6), 16);
+  if (bw) {
+    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#ffffff";
+  }
+  r = (255 - r).toString(16);
+  g = (255 - g).toString(16);
+  b = (255 - b).toString(16);
+  return "#" + padZero(r) + padZero(g) + padZero(b);
+}
 
-  const displayFamilyTree = ({ family, color = null }) => {
-    const familyColor =
-      familyColors[`${family.name}${family.spouse}`] ||
-      familyColors[`${family.spouse}${family.name}`] ||
-      color ||
-      generateNewColor();
+const DisplayFamilyTree = ({ family, familyColors, color = null }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    familyColors[`${family.name}${family.spouse}`] = familyColor;
+  const familyColor =
+    familyColors[`${family.name}${family.spouse}`] ||
+    familyColors[`${family.spouse}${family.name}`] ||
+    color ||
+    generateNewColor();
 
-    if (family.children?.length) {
-      const nameColor = invertColor(familyColor, true);
-      return (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            margin: "auto",
-            border: `1px solid ${nameColor}`,
-            overflow: "auto",
-            whiteSpace: "nowrap",
-            backgroundColor: familyColor,
-            color: nameColor,
-          }}
-        >
-          <div>{family.name}</div>
-          {family?.spouse && <div>{family?.spouse}</div>}
-          <div style={{ display: "flex", margin: "10px 20px 5px" }}>
-            {family.children.map((child, index, children) => (
+  familyColors[`${family.name}${family.spouse}`] = familyColor;
+
+  if (isOpen && family.children?.length) {
+    const nameColor = invertColor(familyColor, true);
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "fit-content",
+          margin: "auto",
+          border: `1px solid ${nameColor}`,
+          overflow: "auto",
+          whiteSpace: "nowrap",
+          backgroundColor: familyColor,
+          color: nameColor,
+          padding: 10,
+          borderRadius: 15,
+        }}
+      >
+        <h4 style={{ padding: 0, margin: 0 }}>
+          {family.childNo} {family.name}
+        </h4>
+        {family?.spouse && <div>{family?.spouse}</div>}
+        <div style={{ display: "flex", margin: 10 }}>
+          {family.children
+            .sort((first, second) => first.childNo - second.childNo)
+            .map((child, index, children) => (
               <div
                 key={child.name}
                 style={{
                   marginRight: index !== children.length - 1 ? "20px" : 0,
                 }}
               >
-                {displayFamilyTree({
-                  family: child,
-                  color: child.isMale ? familyColor : null,
-                })}
+                <DisplayFamilyTree
+                  family={child}
+                  color={child.isMale ? familyColor : null}
+                  familyColors={familyColors}
+                />
               </div>
             ))}
-          </div>
         </div>
-      );
-    }
-    return (
-      <div>
-        <div>{family.name}</div>
-        {family?.spouse && <div>{family?.spouse}</div>}
       </div>
     );
-  };
+  }
+  return (
+    <div>
+      {family.spouse ? (
+        <h4 style={{ padding: 0, margin: 0 }}>
+          {family.childNo} {family.name}
+        </h4>
+      ) : (
+        <div>
+          {family.childNo} {family.name}
+        </div>
+      )}
+      {family?.spouse && (!family.children || !family.children.length) && (
+        <div>{family?.spouse}</div>
+      )}
+      {family.children?.length && (
+        <button
+          style={{ height: 30, marginTop: 5, cursor: "pointer" }}
+          onClick={() => setIsOpen((isOpen) => !isOpen)}
+        >
+          Show Family
+        </button>
+      )}
+    </div>
+  );
+};
 
-  return <div>{displayFamilyTree({ family })}</div>;
+const FamilyTree = () => {
+  const familyColors = {};
+
+  return (
+    <div>
+      <DisplayFamilyTree family={family} familyColors={familyColors} />
+    </div>
+  );
 };
 
 export default FamilyTree;
--- src/constants/familyData.js
+++ src/constants/familyData.js
@@ -1,156 +1,273 @@
-export const family = {
-  name: "Sanyasi Naidu B",
-  spouse: "Varahalamma B",
+const ramakrishnaNaiduFamily = {
+  name: "Ramakrishna Naidu B",
+  spouse: "Chandrudamma B",
   isMale: true,
+  childNo: 1,
   children: [
     {
-      name: "Ramakrishna Naidu B",
-      spouse: "Chandrudamma B",
-      isMale: true,
+      name: "Meena Vavilapalli",
+      spouse: "Appalanaidu V",
+      isMale: false,
+      childNo: 1,
       children: [
         {
-          name: "Meena V",
-          spouse: "Appalanaidu V",
+          name: "Sujatha B",
+          spouse: "Ramanababu B",
           isMale: false,
-          children: [
-            {
-              name: "Sujatha B",
-              spouse: "Ramanababu B",
-              isMale: false,
-              children: [
-                {
-                  name: "Jai Prakash Naidu B",
-                  children: null,
-                  isMale: true,
-                },
-                {
-                  name: "Prudvi Naidu B",
-                  children: null,
-                  isMale: true,
-                },
-              ],
-            },
-            {
-              name: "Aruna",
-              spouse: "Sunil Kumar",
-              isMale: false,
-              children: [
-                {
-                  name: "Hitanvi",
-                  isMale: false,
-                  children: null,
-                },
-                {
-                  name: "Vedanvi",
-                  isMale: false,
-                  children: null,
-                },
-              ],
-            },
-            {
-              name: "Venkatesh V",
-              spouse: "Ramya V",
-              isMale: true,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "Leelaveni K",
-          spouse: "Tatababu K",
-          isMale: false,
-          children: [
-            {
-              name: "Vanitha G",
-              spouse: "Santhosh G",
-              isMale: false,
-              children: [
-                {
-                  name: "Sushmitha G",
-                  isMale: false,
-                  children: null,
-                },
-              ],
-            },
-            {
-              name: "Sai Prashanth K",
-              isMale: true,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "Rambabu B",
-          spouse: "Hyma B",
-          isMale: true,
-          children: [
-            {
-              name: "Sai Siva Teja B",
-              isMale: true,
-              children: null,
-            },
-            {
-              name: "Akhila B",
-              isMale: false,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "Ramanababu B",
-          spouse: "Sujatha B",
-          isMale: true,
+          childNo: 1,
           children: [
             {
               name: "Jai Prakash Naidu B",
-              isMale: true,
               children: null,
+              isMale: true,
+              childNo: 1,
             },
             {
               name: "Prudvi Naidu B",
-              isMale: true,
               children: null,
+              isMale: true,
+              childNo: 2,
             },
           ],
+        },
+        {
+          name: "Aruna",
+          spouse: "Sunil Kumar",
+          isMale: false,
+          childNo: 2,
+          children: [
+            {
+              name: "Hitanvi",
+              isMale: false,
+              children: null,
+              childNo: 1,
+            },
+            {
+              name: "Vedanvi",
+              isMale: false,
+              children: null,
+              childNo: 2,
+            },
+          ],
+        },
+        {
+          name: "Venkatesh V",
+          spouse: "Ramya V",
+          isMale: true,
+          children: null,
+          childNo: 3,
         },
       ],
     },
     {
+      name: "Leelaveni Kuriti",
+      spouse: "Tatababu K",
+      isMale: false,
+      childNo: 2,
+      children: [
+        {
+          name: "Vanitha G",
+          spouse: "Santhosh G",
+          isMale: false,
+          childNo: 1,
+          children: [
+            {
+              name: "Sushmitha G",
+              isMale: false,
+              children: null,
+              childNo: 1,
+            },
+          ],
+        },
+        {
+          name: "Sai Prashanth K",
+          isMale: true,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+    {
+      name: "Rambabu B",
+      spouse: "Hyma B",
+      isMale: true,
+      childNo: 3,
+      children: [
+        {
+          name: "Sai Siva Teja B",
+          isMale: true,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Akhila B",
+          isMale: false,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+    {
+      name: "Ramanababu B",
+      spouse: "Sujatha B",
+      isMale: true,
+      childNo: 4,
+      children: [
+        {
+          name: "Jai Prakash Naidu B",
+          isMale: true,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Prudvi Naidu B",
+          isMale: true,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+  ],
+};
+
+const ramalakshmiFamily = {
+  name: "Ramalakshmi Palavalasa",
+  spouse: "Suryanarayana P",
+  isMale: false,
+  childNo: 3,
+  children: [
+    {
+      name: "Hyma B",
+      spouse: "Rambabu B",
+      isMale: false,
+      childNo: 1,
+      children: [
+        {
+          name: "Sai Siva Teja B",
+          isMale: true,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Akhila B",
+          isMale: false,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+    {
+      name: "Sridevi Vempadapu",
+      spouse: "Swaminaidu V",
+      isMale: false,
+      childNo: 2,
+      children: [
+        {
+          name: "Likitha V",
+          isMale: false,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Anitha V",
+          isMale: false,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+    {
+      name: "VenkataLakshmi Mavudi",
+      spouse: "Srinu M",
+      isMale: false,
+      childNo: 3,
+      children: [
+        {
+          name: "Gowthami M",
+          isMale: false,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Sruthi M",
+          isMale: false,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+    {
+      name: "Ravi Kumar P",
+      spouse: "Sharmila P",
+      isMale: true,
+      childNo: 4,
+      children: [
+        {
+          name: "Jaswanth P",
+          isMale: true,
+          children: null,
+          childNo: 1,
+        },
+        {
+          name: "Revanth P",
+          isMale: true,
+          children: null,
+          childNo: 2,
+        },
+      ],
+    },
+  ],
+};
+
+export const family = {
+  name: "Sanyasi Naidu Bejjipuram",
+  spouse: "Varahalamma B",
+  isMale: true,
+  children: [
+    ramakrishnaNaiduFamily,
+    {
       name: "Achutarao Naidu B",
       spouse: "Papamma B",
+      childNo: 2,
       isMale: true,
       children: [
         {
           name: "Shankar Rao B",
-          spouse: "",
+          spouse: "Bharati B",
           isMale: true,
+          childNo: 1,
           children: [
             {
               name: "Kavya B",
               isMale: false,
               children: null,
+              childNo: 1,
             },
             {
               name: "Chinni B",
               isMale: false,
               children: null,
+              childNo: 2,
             },
             {
               name: "Charan B",
               isMale: true,
               children: null,
+              childNo: 3,
             },
           ],
         },
         {
           name: "Sudha B",
           spouse: "Hyma B",
+          childNo: 2,
           isMale: true,
           children: [
             {
               name: "Pujitha B (Aish)",
               isMale: false,
               children: null,
+              childNo: 1,
             },
           ],
         },
@@ -158,95 +275,33 @@ export const family = {
     },
     {
       name: "Apparao Naidu B",
-      spouse: "",
+      spouse: "Rajeshwari B",
       isMale: true,
+      childNo: 4,
       children: [
         {
+          name: "Padma",
+          spouse: "Narayana Rao",
+          isMale: false,
+          children: null,
+          childNo: 1,
+        },
+        {
           name: "Venu B",
+          spouse: "Kumari B",
           isMale: true,
           children: null,
+          childNo: 2,
         },
         {
           name: "Ganesh B",
+          spouse: "",
           isMale: true,
           children: null,
+          childNo: 3,
         },
       ],
     },
-    {
-      name: "Ramalakshmi P",
-      spouse: "Suryanarayana P",
-      isMale: false,
-      children: [
-        {
-          name: "Hyma B",
-          spouse: "Rambabu B",
-          isMale: false,
-          children: [
-            {
-              name: "Sai Siva Teja B",
-              isMale: true,
-              children: null,
-            },
-            {
-              name: "Akhila B",
-              isMale: false,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "Sridevi V",
-          spouse: "Swaminaidu V",
-          isMale: false,
-          children: [
-            {
-              name: "Likitha V",
-              isMale: false,
-              children: null,
-            },
-            {
-              name: "Anitha V",
-              isMale: false,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "VenkataLakshmi M",
-          spouse: "Srinu M",
-          isMale: false,
-          children: [
-            {
-              name: "Gowthami M",
-              isMale: false,
-              children: null,
-            },
-            {
-              name: "Sruthi M",
-              isMale: false,
-              children: null,
-            },
-          ],
-        },
-        {
-          name: "Ravi Kumar P",
-          spouse: "Sharmila P",
-          isMale: true,
-          children: [
-            {
-              name: "Jaswanth P",
-              isMale: true,
-              children: null,
-            },
-            {
-              name: "Revanth P",
-              isMale: true,
-              children: null,
-            },
-          ],
-        },
-      ],
-    },
+    ramalakshmiFamily,
   ],
 };
